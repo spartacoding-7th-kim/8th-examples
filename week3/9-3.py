6869dee3f4bec47134241913656e7c1a164ce13c
spartacoding-7h-kim <61879561+spartacoding-7h-kim@users.noreply.github.com>
2020-05-24 00:50:15 +0900

    week3

---
+++ week3/9-3.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+# URL을 읽어서 HTML를 받아오고,
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
+
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
+soup = BeautifulSoup(data.text, 'html.parser')
+
+# 선택자를 사용하는 방법 (copy selector)
+print(soup.select('tr'))
+soup.select('.클래스명')
+soup.select('#아이디명')
+
+soup.select('상위태그명 > 하위태그명 > 하위태그명')
+soup.select('상위태그명.클래스명 > 하위태그명.클래스명')
+
+# 태그와 속성값으로 찾는 방법
+soup.select('태그명[속성="값"]')
+
+# 한 개만 가져오고 싶은 경우
+soup.select_one('위와 동일')
