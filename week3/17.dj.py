8c1e687c7244b04258a4d000e5a5f52501a634cd
djay-lee <65063338+djay-lee@users.noreply.github.com>
2020-05-24 18:07:53 +0900

    finish

---
+++ week3/17.dj.py
@@ -0,0 +1,13 @@
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+target_movie = db.movie.find_one({'title':'매트릭스'})
+target_star = target_movie['star']
+print(target_star)
+
+movies = list(db.movie.find({'star':target_star}))
+for movie in movies :
+    print(movie['title'])
+
+db.movie.update_one({'star':target_star},{'$set':{'star':'0'}})
