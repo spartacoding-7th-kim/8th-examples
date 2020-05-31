ed594f8f01e00eea526f14373fb803f8947d45c9
jessey-bee <65078641+jessey-bee@users.noreply.github.com>
2020-05-31 18:35:55 +0900

    Update app-start.py

--- week4/projects_jessey/alonememo/app-start.py
+++ week4/projects_jessey/alonememo/app-start.py
@@ -31,7 +31,7 @@ def saving():
 
     # 2. meta tag를 스크래핑하기
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-    data = requests.get('https://platum.kr/archives/120958', headers = headers)
+    data = requests.get(posting_url, headers = headers)
 
     soup = BeautifulSoup(data.text, 'html.parser')
 
