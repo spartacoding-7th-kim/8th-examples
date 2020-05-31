88ebff927446999225684b78a6cffea6d6f36aaf
graytowhite <65101974+graytowhite@users.noreply.github.com>
2020-05-31 14:46:34 +0900

    test

--- week3/16-1.sero.py
+++ week3/16-1.sero.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbnavermovie                      # 'dbsparta'라는 이름의 db를 만듭니다.
+db = client.dbsparta                # 'dbsparta'라는 이름의 db를 만듭니다.
 
 # URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
--- week3/17-start.sero.py
+++ week3/17-start.sero.py
@@ -1,3 +1,7 @@
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.dbsparta 
+db = client.dbnavermovie
+
+
+target_movie = db.users.find_one({'title':'매트릭스'})
+print (target_movie['star'])
