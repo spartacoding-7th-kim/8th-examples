04aca8f26d45254d254df38f21b798e9ff0d8822
esgun <radiovideostar@gmail.com>
2020-06-07 12:55:38 +0900

    lee

--- week3/17-start.lee.py
+++ week3/17-start.lee.py
@@ -1,13 +1,15 @@
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.dbnavermovie
 
-## 코딩 할 준비 ##
-
-target_movie = db.movies.find_one({'title': '매트릭스'})
+target_movie = db.movie.find_one({'title': '매트릭스'})
 target_star = target_movie['star']
+# print(target_movie['star'])
 
 movies = list(db.movie.find({'star': target_star}))
-
+print(movies)
 for movie in movies:
     print(movie['title'])
+
+
+db.movie.update_many({'star': target_star}, {'$set': {'star:0'}})
