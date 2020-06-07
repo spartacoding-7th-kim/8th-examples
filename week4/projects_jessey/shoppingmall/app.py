57253d3a9a4b3039d48485536415fc8bc406968b
jessey-bee <65078641+jessey-bee@users.noreply.github.com>
2020-06-07 16:50:21 +0900

    jessey

--- week4/projects_jessey/shoppingmall/app.py
+++ week4/projects_jessey/shoppingmall/app.py
@@ -6,22 +6,22 @@ from bs4 import BeautifulSoup
 
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
-db = client.dbsparta                      # 'dbsparta'라는 이름의 db를 만듭니다.
+db = client.wheretogo                      # 'dbsparta'라는 이름의 db를 만듭니다.
 
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index-start.html')
+   return render_template('index.html')
 
-@app.route('/memo', methods=['GET'])
+@app.route('/order', methods=['GET'])
 def listing():
     # 1. 모든 document 찾기 & _id 값은 출력에서 제외하기
-    articles = list(db.metas.find({}, {'_id':0}))
+    orders = list(db.metas.find({}, {'_id':0}))
     # 2. articles라는 키 값으로 영화정보 내려주기
-    return jsonify({'articles':articles, 'result':'success', 'msg':'GET 연결되었습니다!'})
+    return jsonify({'orders': orders, 'result':'success', 'msg':'GET 연결되었습니다!'})
 
 ## API 역할을 하는 부분
-@app.route('/memo', methods=['POST'])
+@app.route('/order', methods=['POST'])
 def saving():
     posting_url = request.form['posting_url_key']
     posting_comment = request.form['posting_comment_key']
